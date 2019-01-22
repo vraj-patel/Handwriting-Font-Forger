8c150ecc6233112e31b75052393c940574fdf120
vraj-patel <35196999+vraj-patel@users.noreply.github.com>
2019-01-22 00:20:59 -0500

    Add files via upload

---
+++ ImitateHandWriting/ImitateHandWriting/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ImitateHandWriting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit The Character Above";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(326, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(352, 66);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(326, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(417, 178);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert To Handwriting";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(323, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Please Enter The Text To Convert";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(774, 365);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImitateHandWriting/ImitateHandWriting/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImitateHandWriting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap b1 = new Bitmap(200, 200);
+        string[] allSymbols = { "}","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "'", "(", ")", ".", "{", "}", ":", ";", "+", "/", "*", "-", "&", "^", "$", "#", "@", "!" };
+
+        int x = 0;
+        SolidBrush brush = new SolidBrush(Color.Black);
+       
+
+        Bitmap btmp = new Bitmap(200, 200);
+
+        List<Bitmap> allBitmaps = new List<Bitmap>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            textBox1.Text = allSymbols[x]; 
+            
+        }
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                allBitmaps.Add(btmp);
+                btmp = new Bitmap(200, 200);
+                pictureBox1.Image = btmp;
+                Bitmap bip = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+
+                Graphics g = Graphics.FromImage(btmp);
+                Graphics g2 = Graphics.FromImage(bip);
+
+                x += 1;
+
+                if (x > allSymbols.Length - 1)
+                {
+                    textBox1.Text = "Please enter your text";
+
+                }
+                else
+                {
+                    textBox1.Text = allSymbols[x];
+                }
+
+            }catch(Exception g)
+            {
+
+            }
+            
+            
+        }
+
+        Point lastPoint = Point.Empty;
+        bool isMouseDown;
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (lastPoint != null && isMouseDown)
+            {
+                if (pictureBox1.Image == null)   
+                {                                         
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox2.Height);
+
+                    pictureBox1.Image = bmp;
+                }
+
+                Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+                g.DrawLine(new Pen(Color.Black, 20), lastPoint, e.Location);
+                
+
+                pictureBox1.Invalidate();
+
+                lastPoint = e.Location;
+
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = e.Location;
+
+            isMouseDown = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+
+            lastPoint = Point.Empty;
+        }
+
+        Bitmap btmp3; 
+
+        private void printHandWriting (int size)
+        {
+            try
+            {
+                if (size == 0)
+                {
+                    btmp3 = new Bitmap(30, 100);
+                }
+                else
+                    btmp3 = new Bitmap(size * 30, 100); 
+
+                pictureBox2.Width = size * 30;
+
+                Graphics g = Graphics.FromImage(btmp3);
+
+                string hello = richTextBox1.Text;
+                int xpos = 0;
+
+                for (int x = 0; x <= richTextBox1.Text.Length - 1; x++)
+                {
+
+                    for (int y = 0; y <= allSymbols.Length - 1; y++)
+                    {
+                        if (hello.Substring(x, 1) == allSymbols[y])
+                        {
+                            allBitmaps[y] = new Bitmap(allBitmaps[y], 20, 20);
+                            g.DrawImage(allBitmaps[y], xpos, 30);
+                            break;
+                        }
+
+                    }
+                    xpos += 15;
+
+                }
+
+                pictureBox2.Image = btmp3;
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int size = richTextBox1.Text.Length; 
+            printHandWriting(size); 
+        }
+
+
+    }
+}
